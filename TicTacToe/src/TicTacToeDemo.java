82db137971d81d5c7f8dd96430947561e48b31d1
Marcin Szostak <marcinszostak@Marcins-MacBook-Air.local>
2022-12-30 09:07:38 +0100

    Implemented a method for printing a game grid

---
+++ TicTacToe/src/TicTacToeDemo.java
@@ -0,0 +1,9 @@
+import TicTacToe.*;
+
+public class TicTacToeDemo {
+    public static void main(String[] args) {
+        TicTacToeController ticTacToeController = new TicTacToeController();
+
+        ticTacToeController.printGameGrid();
+    }
+}
